4067cf288b789de70dd33ee55d5ee74e528c48f7
William Hwang <willhwang90@gmail.com>
2022-05-12 13:06:50 -0400

    added if statements to remove null values

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 document.addEventListener('DOMContentLoaded', displayWebsitePresets)
 
 function displayWebsitePresets(){
-    clickName.innerText = "Vodka Cocktails"
+    clickName.innerText = "Cocktail Dictionary: 'B'"
     cocktailImage.src = `https://upload.wikimedia.org/wikipedia/commons/thumb/1/13/Un_bar_aux_Folies-Berg%C3%A8re_d%27E._Manet_%28Fondation_Vuitton%2C_Paris%29_%2833539037428%29.jpg/1920px-Un_bar_aux_Folies-Berg%C3%A8re_d%27E._Manet_%28Fondation_Vuitton%2C_Paris%29_%2833539037428%29.jpg`
-    clickDescription.innerText = "Description of the Website Function, tagline, or something"
+    clickDescription.innerText = "For the community of people who love both vodka and the letter B"
 }
 
 function getCocktails (){
@@ -31,17 +31,19 @@ console.log(ingredientsList)
 function detailsFinder(arr, string){
     let drinkDetails = arr.find(item => item.idDrink === string)
     let ingli1 = document.createElement('li');
-    ingli1.innerHTML = drinkDetails.strIngredient1
-   console.log(drinkDetails.strIngredient1);
+    ingli1.textContent =drinkDetails.strMeasure1+ " "+drinkDetails.strIngredient1
+   console.log(drinkDetails.strMeasure1);
     let ingli2 = document.createElement('li'); 
-    ingli2.textContent = drinkDetails.strIngredient2
-    console.log(drinkDetails.strIngredient2);
+    ingli2.textContent = drinkDetails.strMeasure2+" "+drinkDetails.strIngredient2
+    console.log(drinkDetails.strMeasure2);
     let ingli3 = document.createElement('li')
-    ingli3.textContent = drinkDetails.strIngredient3;
-    console.log(drinkDetails.strIngredient3);
+    if(drinkDetails.strIngredient4 !== null){
+    ingli3.textContent = drinkDetails.strMeasure3+" "+drinkDetails.strIngredient3;}
+    console.log(drinkDetails.strMeasure3);
     let  ingli4 = document.createElement('li')
-    ingli4.textContent = drinkDetails.strIngredient4
-    console.log(drinkDetails.strIngredient4);
+   if(drinkDetails.strIngredient4 !== null){
+    ingli4.textContent = drinkDetails.strMeasure4+" "+drinkDetails.strIngredient4;}
+    console.log(drinkDetails.strMeasure4);
     ingredientsList.append(ingli1, ingli2, ingli3, ingli4);
   }
 function renderCocktails(data){
@@ -69,7 +71,6 @@ function renderCocktails(data){
         likeAmt.textContent = count;
         likeButton.addEventListener('click', function(){
             count = count + 1;
-            likeAmt.innerHTML = count;
             likeAmt.textContent = count;
 })
     
