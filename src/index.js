27a99dd1f418db896e18ff1548ebc08dba3b984f
William Hwang <willhwang90@gmail.com>
2022-05-12 13:20:47 -0400

    updated pointer and added instructions.

--- src/index.js
+++ src/index.js
@@ -32,19 +32,18 @@ function detailsFinder(arr, string){
     let drinkDetails = arr.find(item => item.idDrink === string)
     let ingli1 = document.createElement('li');
     ingli1.textContent =drinkDetails.strMeasure1+ " "+drinkDetails.strIngredient1
-   console.log(drinkDetails.strMeasure1);
+   console.log(drinkDetails.strInstructions);
     let ingli2 = document.createElement('li'); 
     ingli2.textContent = drinkDetails.strMeasure2+" "+drinkDetails.strIngredient2
-    console.log(drinkDetails.strMeasure2);
     let ingli3 = document.createElement('li')
     if(drinkDetails.strIngredient4 !== null){
     ingli3.textContent = drinkDetails.strMeasure3+" "+drinkDetails.strIngredient3;}
-    console.log(drinkDetails.strMeasure3);
     let  ingli4 = document.createElement('li')
    if(drinkDetails.strIngredient4 !== null){
     ingli4.textContent = drinkDetails.strMeasure4+" "+drinkDetails.strIngredient4;}
-    console.log(drinkDetails.strMeasure4);
-    ingredientsList.append(ingli1, ingli2, ingli3, ingli4);
+    let instructli = document.createElement('li');
+    instructli.textContent= 'Instructions: ' +drinkDetails.strInstructions
+    ingredientsList.append(ingli1, ingli2, ingli3, ingli4, instructli);
   }
 function renderCocktails(data){
     // for(const item of data.drinks){
